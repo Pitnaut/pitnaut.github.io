af1ff9db15b8a5a890d5e024e46ef48bbbe5d856
Pitnaut <114064538+Pitnaut@users.noreply.github.com>
2023-01-26 10:41:27 +0100

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-const profilePhoto = document.getElementById("profile-photo");
-
-let changeImage = () => profilePhoto.style.transition = "all .5s";
-profilePhoto.addEventListener('mouseover', changeImage);
